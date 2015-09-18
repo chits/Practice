0006649f99c2a1e84368dc67c456a28f13d0ada4
Chitrali Rai <Chitrali@Abhisheks-Air.home>
2015-09-17 21:18:54 -0400

    First commit

---
+++ src/com/chitrali/practice/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.chitrali.practice;
+
+/**
+ * @author Chitrali
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
